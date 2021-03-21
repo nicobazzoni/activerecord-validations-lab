6c16dc6c6f07836f01e59ba2e669575686fc43c5
Nico Bazzoni <you@example.com>
2021-03-21 17:41:56 -0400

    commit tree 673a644fcfdedd4e2c8a55610236b0c675d8c091

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 300 }
+  validates :summary, length: { maximum: 200 }
+  validates :category, inclusion: { in: %w( Fiction Non-Fiction), 
+
+  message: "%{value} not a valid category. Category must either be Fiction or Non-Fiction."}
+  validate :click_bait
+
+  def click_bait
+    if title && !title.include?("Won't Believe" || "Secret" ||
+      "Top [number]" || "Guess")
+      errors.add(:title, "Must be clickbait-y")
+    end
+  end
 end
